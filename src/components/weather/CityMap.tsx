b59526b88eb9c565c094e2f031cd8f4c58f702ef
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-03-30 19:42:02 +0530

    build err fix

--- src/components/weather/CityMap.tsx
+++ src/components/weather/CityMap.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
+import React, { useEffect, useRef, useState } from 'react';
 import { CityWithCoordinates } from '../../types/weather';
 import WeatherTooltip from './WeatherTooltip';
 
@@ -40,21 +39,21 @@ const CityMap: React.FC<CityMapProps> = ({ selectedCity, cities }) => {
             .translate([width / 2, height / 2]);
 
         // Create a path generator
-        const path = d3.geoPath().projection(projection);
+        // const path = d3.geoPath().projection(projection);
 
         // Simplify and draw world map with dark styling
         d3.json("https://unpkg.com/world-atlas@2.0.2/countries-110m.json").then((data: any) => {
             if (!data || !data.objects) return;
 
-            const countries = topojson.feature(data, data.objects.countries);
+            // const countries = topojson.feature(data, data.objects.countries);
 
             // Draw countries
             svg.selectAll("path.country")
-                .data(countries.features)
+                // .data(countries.features)
                 .enter()
                 .append("path")
                 .attr("class", "country")
-                .attr("d", path)
+                // .attr("d", path)
                 .attr("fill", "#2A2E35")
                 .attr("stroke", "#1E293B")
                 .attr("stroke-width", 0.5);
